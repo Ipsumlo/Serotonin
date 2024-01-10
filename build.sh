0248aaae4657795a07af86ee5ab77f5c2c9c755d
Ipsumlo <154028999+Ipsumlo@users.noreply.github.com>
2024-01-10 02:12:27 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -e # why wasn't this there earlier – bomberfish
 echo "Building IPA"
-xcodebuild clean build -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED="NO" SDKROOT="iphoneos15.5"
+#xcodebuild clean build -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED="NO" SDKROOT="iphoneos15.5"
 echo "done building"
 cd RootHelperSample
 ldid -Sentitlements.plist -Cadhoc ../usprebooter/ldid
 ldid -Sentitlements.plist -Cadhoc ../usprebooter/fastPathSign
 gmake -j$(sysctl -n hw.ncpu)
 ldid -Sentitlements.plist -Cadhoc .theos/obj/debug/arm64/trolltoolsroothelper
-mv .theos/obj/debug/arm64/trolltoolsroothelper ../build/Release-iphoneos15.5/usprebooter.app/trolltoolsroothelper
-cd ../build/Release-iphoneos15.5
+mv .theos/obj/debug/arm64/trolltoolsroothelper ../build/Release-iphoneos/usprebooter.app/trolltoolsroothelper
+cd ../build/Release-iphoneos
 rm -rf Payload
 rm -rf FUCK.tipa
 mkdir Payload
